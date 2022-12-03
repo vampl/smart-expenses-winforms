3c0d577d848189bf4eb4ccc142076b9d75a4923f
Vitalik Barabash <vitalik.barabash.2002@gmail.com>
2022-12-03 03:22:47 +0200

    Implemented Locator (Transfer Database Collector). Refactoring and functionality of the main page.

---
+++ SmartExpense/Locator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SmartExpense
+{
+    // class to combine services. 
+    public static class Locator
+    {
+        private static readonly Dictionary<Type, object> Services = new Dictionary<Type, object>();
+        
+        // getting service by it's type.
+        public static T GetService<T>()
+        {
+            return (T)Services[typeof(T)];
+        }
+        
+        // bind service to locator.
+        public static void Register<T>(T service)
+        {
+            Services[typeof(T)] = service;
+        }
+        
+        // clear locator.
+        public static void Reset()
+        {
+            Services.Clear();
+        }
+    }
+}
